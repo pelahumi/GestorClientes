33b3f11badd7e562ebe8247c265170d6fac213dd
Pelayo <phuermij@myuax.com>
2022-10-06 19:05:14 +0200

    mas csv

--- Gestor/database.py
+++ Gestor/database.py
@@ -20,6 +20,13 @@ class Clientes():
             lista.append(cliente)
 
     @staticmethod
+    def guardar():
+        with open("clientes.csv", "w", newline="\n") as fichero:
+            writer = csv.writer(ficehro, delimiter= ";")
+            for c in Clientes.lista:
+                writer.writerow((c.dni, c.nombre, c.apellido))
+
+    @staticmethod
     def buscar(dni):
         for cliente in Clientes.lista:
             if cliente.dni == dni:
@@ -29,6 +36,7 @@ class Clientes():
     def crear(dni, nombre, apellido):
         cliente = Cliente(dni, nombre, apellido)
         Clientes.lista.append(cliente)
+        Clientes.guardar()
         return cliente
     
     @staticmethod
@@ -37,6 +45,7 @@ class Clientes():
             if cliente.dni == dni:
                 Clientes.lista[i].nombre = nombre
                 Clientes.lista[i].apellido = apellido
+                Clientes.guardar()
                 return Clientes.lista[i]
     
     @staticmethod
@@ -44,4 +53,5 @@ class Clientes():
         for i, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
                 cliente = Clientes.lista.pop(i)
+                Clientes.guardar()
                 return cliente
