e19e441a15c96a573ffe8e81f4e4839808939266
Pelayo <phuermij@myuax.com>
2022-10-06 18:59:52 +0200

    menu

--- Gestor/menu.py
+++ Gestor/menu.py
@@ -31,11 +31,14 @@ def iniciar():
 
         if opcion == "3":
             print("Añadiendo un cliente...\n")
-            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
-            nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
-            apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
-            db.Clientes.crear(dni, nombre, apellido)
-            print("Cliente añadido correctamente")
+            while 1:
+                dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+                if helpers.dni_valido(dni, db.Clientes.lista):
+                    break
+                nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
+                apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
+                db.Clientes.crear(dni, nombre, apellido)
+                print("Cliente añadido correctamente")
 
         if opcion == "4":
             print("Modificando un cliente...\n")
